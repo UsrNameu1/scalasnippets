1a21d61cd40f8bffd5cd92618abe3fe88be3569b
UsrNameu1 <tea_room_1028@yahoo.co.jp>
2014-06-24 23:55:38 +0900

    Add chap6 Rational class

---
+++ src/main/scala/copbook/chap6/Rational.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2007-2008 Artima, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Example code from:
+ *
+ * Programming in Scala (First Edition, Version 6)
+ * by Martin Odersky, Lex Spoon, Bill Venners
+ *
+ * http://booksites.artima.com/programming_in_scala
+ */
+
+package copbook.chap6
+
+
+class Rational(n: Int, d:Int) {
+  require(d != 0)
+
+  private val g = gcd(n.abs, d.abs)
+
+  val numer: Int = n / g
+  val denom: Int = d / g
+
+  def this(n: Int) = this(n, 1) // auxiliary constructors
+
+  def + (toRational: Rational): Rational =
+    new Rational(
+      numer * toRational.denom + toRational.numer * denom,
+      denom * toRational.denom
+    )
+
+  def * (toRational: Rational): Rational =
+    new Rational(numer * toRational.numer, denom * toRational.denom)
+
+  override def toString = numer + "/" + denom
+
+  private def gcd(a: Int, b: Int): Int =
+    if (b == 0) a else gcd(b, a % b)
+}
---
+++ src/main/scala/copbook/chap6/RationalDriver.scala
@@ -0,0 +1,13 @@
+package copbook.chap6
+
+/**
+ * Created by yad on 2014/06/24.
+ */
+object RationalDriver {
+   def main(args: Array[String]) {
+     val x = new Rational(1, 2)
+     println(x)
+     val y = new Rational(2, 3)
+     println(x + y)
+   }
+}
