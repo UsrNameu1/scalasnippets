c36843a2402adc81b24543077681c00e699fe483
UsrNameu1 <tea_room_1028@yahoo.co.jp>
2014-07-21 23:56:23 +0900

    Add 10.9

--- src/main/scala/copbook/chap10/Element.scala
+++ src/main/scala/copbook/chap10/Element.scala
@@ -30,6 +30,10 @@ abstract class Element {
   def contents: Array[String]
   val height: Int = contents.length
   val width: Int = if (height == 0) 0 else contents(0).length
+
+  def demo() {
+    println("Element's implementation invoked")
+  }
 }
 
 //class ArrayElement(conts: Array[String]) extends Element {
@@ -38,9 +42,26 @@ abstract class Element {
 
 class ArrayElement (
   val contents: Array[String]
+
+  override def demo() {
+    println("ArrayElement's implementation invoked")
+  }
 ) extends Element
 
 class LineElement(s: String) extends ArrayElement(Array(s)) {
   override def width = s.length
   override def height = 1
+
+  override def demo() {
+    println("LineElement's implementation invoked")
+  }
+}
+
+class UniformElement(
+  ch: Char,
+  override val width: Int,
+  override val height: Int
+) extends Element {
+  private val line = ch.toString * width
+  def contents = Array.fill(height)(line)
 }
