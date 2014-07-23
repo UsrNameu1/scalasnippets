811f53c6828ed15dd7cb403913dc011b8adaf97b
UsrNameu1 <tea_room_1028@yahoo.co.jp>
2014-07-23 23:59:35 +0900

    Add 10.11-12

--- src/main/scala/copbook/chap10/Element.scala
+++ src/main/scala/copbook/chap10/Element.scala
@@ -28,8 +28,20 @@ package copbook.chap10
  */
 abstract class Element {
   def contents: Array[String]
-  val height: Int = contents.length
-  val width: Int = if (height == 0) 0 else contents(0).length
+  def height: Int = contents.length
+  def width: Int = if (height == 0) 0 else contents(0).length
+
+  def above(that: Element): Element =
+    new ArrayElement(this.contents ++ that.contents)
+
+  def beside(that: Element): Element =
+    new ArrayElement(
+      for (
+        (line1, line2) <- this.contents zip that.contents
+      ) yield line1 + line2
+    )
+
+  override def toString = contents mkString "\n"
 
   def demo() {
     println("Element's implementation invoked")
@@ -48,7 +60,9 @@ final class ArrayElement (
   }
 }
 
-class LineElement(s: String) extends ArrayElement(Array(s)) {
+class LineElement(s: String) extends Element {
+  val contents = Array(s)
+
   override def width = s.length
   override def height = 1
 
