55210676e4e99bea74d990a38a8e5c4d920be488
UsrNameu1 <tea_room_1028@yahoo.co.jp>
2014-07-27 23:48:56 +0900

    Add 10.13-16

--- src/main/scala/copbook/chap10/Element.scala
+++ src/main/scala/copbook/chap10/Element.scala
@@ -26,56 +26,76 @@ package copbook.chap10
 /**
  * Created by yad on 2014/07/17.
  */
+
 abstract class Element {
+  import Element.elem
+
   def contents: Array[String]
   def height: Int = contents.length
   def width: Int = if (height == 0) 0 else contents(0).length
 
-  def above(that: Element): Element =
-    new ArrayElement(this.contents ++ that.contents)
+  def above(that: Element): Element = {
+    val extendedThis = this widen that.width
+    val extendedThat = that widen this.width
+    elem(extendedThis.contents ++ extendedThat.contents)
+  }
 
-  def beside(that: Element): Element =
-    new ArrayElement(
-      for (
-        (line1, line2) <- this.contents zip that.contents
-      ) yield line1 + line2
+  def beside(that: Element): Element = {
+    val extendedThis = this heighten that.height
+    val extendedThat = that heighten this.height
+    elem(
+      for ((line1, line2) <- extendedThis.contents zip extendedThat.contents)
+      yield line1 + line2
     )
+  }
+
+  def widen(w: Int): Element =
+    if (w <= width) this
+    else {
+      val left = elem(' ', (w - width) / 2, height)
+      val right = elem(' ', w - width - left.width, height)
+      left beside this beside right
+    }
+
+  def heighten(h: Int): Element =
+    if (h <= height) this
+    else {
+      val top = elem(' ', width, (h - height) / 2)
+      val bot = elem(' ', width, h - height - top.height)
+      top above this above bot
+    }
 
   override def toString = contents mkString "\n"
 
-  def demo() {
-    println("Element's implementation invoked")
-  }
 }
 
-//class ArrayElement(conts: Array[String]) extends Element {
-//  def contents: Array[String] = conts
-//}
+object Element {
 
-final class ArrayElement (
-  val contents: Array[String]
-) extends Element {
-  override def demo() {
-    println("ArrayElement's implementation invoked")
+  private class ArrayElement (
+                               val contents: Array[String]
+                               ) extends Element
+
+  private class LineElement(s: String) extends Element {
+    val contents = Array(s)
+
+    override def width = s.length
+    override def height = 1
   }
-}
 
-class LineElement(s: String) extends Element {
-  val contents = Array(s)
-
-  override def width = s.length
-  override def height = 1
-
-  override def demo() {
-    println("LineElement's implementation invoked")
+  private class UniformElement (
+                                 ch: Char,
+                                 override val width: Int,
+                                 override val height: Int
+                                 ) extends Element {
+    private val line = ch.toString * width
+    def contents = Array.fill(height)(line)
   }
-}
 
-class UniformElement(
-  ch: Char,
-  override val width: Int,
-  override val height: Int
-) extends Element {
-  private val line = ch.toString * width
-  def contents = Array.fill(height)(line)
+  def elem(contents: Array[String]): Element =
+    new ArrayElement(contents)
+  def elem(chr: Char, width: Int, height: Int): Element =
+    new UniformElement(chr, width, height)
+  def elem(line: String): Element =
+    new LineElement(line)
+
 }
