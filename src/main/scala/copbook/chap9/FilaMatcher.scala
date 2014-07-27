50a206ae403ba5bcf7e57f1a5b48c7fcd993efa8
UsrNameu1 <tea_room_1028@yahoo.co.jp>
2014-07-27 23:49:32 +0900

    minor changes in chap9

--- src/main/scala/copbook/chap9/FilaMatcher.scala
+++ src/main/scala/copbook/chap9/FilaMatcher.scala
@@ -30,7 +30,7 @@ object FilaMatcher {
   private def filesHere = new java.io.File(".").listFiles
 
   private def filesMatching(matcher: String => Boolean) =
-    for (file <- filesHere: if matcher(file.getName))
+    for (file <- filesHere if matcher(file.getName))
       yield file
 
   def filesEnding(query: String) =
